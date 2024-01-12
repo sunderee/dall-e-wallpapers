e618907da5037896d46cf23f2c8fc2a92e54201e
Peter Aleksander Bizjak <peter.aleksander@bizjak.dev>
2024-01-12 13:14:54 +0100

    Add images

--- _pages/index.blade.php
+++ _pages/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
     <main id="content" class="mx-auto max-w-7xl py-16 px-8">
-        <h1 class="text-center text-3xl font-bold">Hello World!</h1>
+        <h1 class="text-center text-3xl font-bold">Gallery</h1>
     </main>
 
 @endsection
