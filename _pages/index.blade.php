331111a198258b705d02c5989c204536a8d428ee
Peter Aleksander Bizjak <peter.aleksander@bizjak.dev>
2024-01-20 21:59:31 +0100

    Able to display images in the gallery now

--- _pages/index.blade.php
+++ _pages/index.blade.php
@@ -3,6 +3,15 @@
 
     <main id="content" class="mx-auto max-w-7xl py-16 px-8">
         <h1 class="text-center text-3xl font-bold">Gallery</h1>
+        <div class="grid grid-cols-1 gap-4 mt-8">
+            @php
+                $images = Illuminate\Support\Facades\File::files(base_path('_media'));
+            @endphp
+
+            @foreach($images as $image)
+                <img src="/_media/{{ $image->getFilename() }}" alt="..." class="mx-auto">
+            @endforeach
+        </div>
     </main>
 
 @endsection
